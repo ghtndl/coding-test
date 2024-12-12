bc2d9bd9a1fbe56cdea4fba639e3132c18ae585c
ghtndl <leesooho0406@gmail.com>
2024-12-12 18:24:52 +0900

    Solve: P1546

--- src/codingtest/datastructure/arr/P1546.java
+++ src/codingtest/datastructure/arr/P1546.java
@@ -1,5 +1,6 @@
 package codingtest.datastructure.arr;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1546 {
@@ -19,3 +20,21 @@ public class P1546 {
         System.out.println(sum*100.0/max/N);
     }
 }
+// 이것도 가능
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//    double[] arr = new double[n];
+//    for (int i = 0; i < n; i++) {
+//        arr[i] = sc.nextInt();
+//    }
+//    Arrays.sort(arr);
+//    for(int i = 0; i < n; i++){
+//        arr[i] = arr[i]/arr[n-1]*100;
+//    }
+//    double sum = 0;
+//    for (int i = 0; i < n; i++) {
+//        sum += arr[i];
+//    }
+//    System.out.println(sum/n);
+//}
