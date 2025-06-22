49266d37aab0fee919447fca61066d8649b2da19
ghtndl <leesooho0406@gmail.com>
2025-06-22 23:51:54 +0900

    [BOJ][U] 2309 일곱 난쟁이

--- practice.cpp
+++ practice.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v;
+int N;
+int cnt;
+void solve(int N) {
+  int a = 0, i = N;
+  while (i > 0) {
+    a += i;
+    i /= 2;
+    cnt++;
+  }
+  cout << a << '\n';
+  cout << cnt << '\n';
+}
+
 int main() {
-  int n = 100;
-  int b = 2;
-  while (n > 1) {
-    v.push_back(n % b);
-    n /= b;
-  }
-  if (n == 1) v.push_back(1);
-  reverse(v.begin(), v.end());
-  for (int a : v) {
-    if (a >= 10)
-      cout << char(a + 55);
-    else
-      cout << a;
-  }
+  cin >> N;
+  solve(N);
   return 0;
 }
