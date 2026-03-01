c45925fa2a368b2eb09e0e0dab6111ebf64a9fc5
Suho Lee <leesooho0406@gmail.com>
2026-03-01 23:46:19 +0900

    [BOJ][U] 13244 Tree

---
+++ Unsolved/u_13244.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, n, m, a, b, visited[1004], cnt;
+vector<int> adj[1004];
+
+void fastIO() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+}
+
+void dfs(int here) {
+  visited[here] = 1;
+  for (int there : adj[here]) {
+    if (!visited[there]) {
+      dfs(there);
+    }
+  }
+  return;
+}
+
+int main() {
+  fastIO();
+  cin >> t;
+  while (t--) {
+    for (int i = 0; i < 1004; i++) adj[i].clear();
+    fill(visited, visited + 1004, 0);
+    cnt = 0;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+      cin >> a >> b;
+      adj[a].push_back(b);
+      adj[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++) {
+      if (!visited[i]) {
+        dfs(i);
+        cnt++;
+      }
+    }
+    if (m == n - 1 && cnt == 1) {
+      puts("tree");
+    } else {
+      puts("graph");
+    }
+  }
+  return 0;
+}
